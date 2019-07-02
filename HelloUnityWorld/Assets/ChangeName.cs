ea6d833af024c7b956cb7695c9795673c25769ea
Cynpaii <cynthiachu@knights.ucf.edu>
2019-07-02 01:19:13 -0400

    First commit

---
+++ HelloUnityWorld/Assets/ChangeName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChangeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown("space"))
+        {
+            
+            textmeshPro.SetText("Hello World from Cynthia Chu"); 
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+    }
+}
